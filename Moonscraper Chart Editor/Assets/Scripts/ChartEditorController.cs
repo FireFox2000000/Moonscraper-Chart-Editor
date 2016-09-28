d8ba83c644f25bc0b00f01301d8560de2eb598e9
Alexander Ong <FireFox2000000@gmail.com>
2016-09-28 20:35:11 +1000

    Full scrollbar implemented and working with real charts.

--- Moonscraper Chart Editor/Assets/Scripts/ChartEditorController.cs
+++ Moonscraper Chart Editor/Assets/Scripts/ChartEditorController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
@@ -14,21 +15,25 @@ public class ChartEditorController : MonoBehaviour {
 
     float scrollDelta = 0;
     Song currentSong;
+    Chart currentChart;
 
     // Use this for initialization
     void Start () {
         currentSong = new Song();
+        currentChart = currentSong.expert_single;
         initPos = transform.position;
         scrollBar.value = 0;
+        UpdatePosBasedScrollValue();
     }
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
 	void Update () {
         if (scrollDelta == 0)
         {
             scrollDelta = Input.mouseScrollDelta.y;
         }
 
+        // Position changes scroll bar value
         if (scrollDelta != 0)
         {
             // Mouse scroll movement
@@ -37,31 +42,52 @@ public class ChartEditorController : MonoBehaviour {
             if (transform.position.y < initPos.y)
                 transform.position = initPos;
 
-            // Update the content height
-            UpdateContentHeight(transform.position.y + Camera.main.orthographicSize - initPos.y);
-
-            // Update the scroll value
-            scrollBar.value = (transform.position.y - initPos.y) / (content.sizeDelta.y * content.transform.lossyScale.y);
+            UpdateScrollValueBasedPos();
         }
 
-        // Update for grabbing the scroll bar
+        // Scroll bar value changes position
         else
         {
-            // Scales the ditance moved to the size of the content height
-            float distanceScale = 1 - 2.0f * Camera.main.orthographicSize / (content.rect.height * content.lossyScale.y);
-
-            // Grabbing the scrollbar
-            float pos = content.rect.height * content.lossyScale.y * scrollBar.value;// * distanceScale;
-            
-            // Apply the position
-            transform.position = new Vector3(transform.position.x, pos + initPos.y, transform.position.z);
-
-            // Update the content height
-            UpdateContentHeight(transform.position.y + Camera.main.orthographicSize - initPos.y);
-        } 
+            UpdatePosBasedScrollValue();
+        }
     }
 
-    void UpdateContentHeight(float maxHeight)
+    void UpdateScrollValueBasedPos()
+    {
+        UpdateContentHeight();
+
+        // Update the scroll value
+        scrollBar.value = (transform.position.y - initPos.y) / (content.sizeDelta.y * content.transform.lossyScale.y);
+    }
+
+    void UpdatePosBasedScrollValue()
+    {
+        // Scales the ditance moved to the size of the content height
+        float distanceScale = 1 - 2.0f * Camera.main.orthographicSize / (content.rect.height * content.lossyScale.y);
+        
+        // Grabbing the scrollbar
+        float pos = content.rect.height * content.lossyScale.y * scrollBar.value;
+
+        // Apply the position
+        transform.position = new Vector3(transform.position.x, pos + initPos.y, transform.position.z);
+
+        UpdateContentHeight();
+    }
+
+    void UpdateContentHeight()
+    {
+        // Update the content height
+        float user_pos = transform.position.y + Camera.main.orthographicSize - initPos.y;
+        float max = user_pos;
+        if (currentChart != null)
+        {
+            if (currentChart.Length > 0 && currentChart[currentChart.Length - 1].position * 0.01f > user_pos)
+                max = currentChart[currentChart.Length - 1].position * 0.01f;
+            ContentHeight(content, max);
+        }
+    }
+
+    void ContentHeight(RectTransform content, float maxHeight)
     {
         const float MINHEIGHT = 300;
         float height = maxHeight / content.transform.lossyScale.y;
@@ -84,6 +110,11 @@ public class ChartEditorController : MonoBehaviour {
 
     public void LoadChart()
     {
+        StartCoroutine(_LoadChart());
+    }
+
+    public IEnumerator _LoadChart()
+    {
         try
         {
             currentSong = new Song(UnityEditor.EditorUtility.OpenFilePanel("Load Chart", "", "chart"));
@@ -94,20 +125,22 @@ public class ChartEditorController : MonoBehaviour {
                 Destroy(note);
             }
 
+            currentChart = currentSong.expert_single;
+
             // Add notes for current chart
-            CreateChartObjects(currentSong.expert_single);
+            CreateChartObjects(currentChart); 
 
             songNameText.text = currentSong.name;
 
             transform.position = initPos;
-
-            Debug.Log(Utility.BinarySearchChartExactNote(currentSong.expert_single[15], currentSong.expert_single.ToArray()));
         }
         catch (System.Exception e)
         {
             Debug.LogError(e.Message);
             // Most likely closed the window explorer, just ignore for now.
         }
+        yield return null;
+        scrollBar.value = 0; 
     }
 
     void CreateChartObjects (Chart chart, GameObject notePrefab)
