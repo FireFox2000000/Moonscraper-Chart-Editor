b8e6ef4f753036032bd86f8849c68e3a10def70d
Alexander Ong <FireFox2000000@gmail.com>
2017-01-09 15:45:08 +1100

    Added guards onto gameplay mode and fret indicators.

--- Moonscraper Chart Editor/Assets/Scripts/Gameplay/Indicators.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Gameplay/Indicators.cs
@@ -10,29 +10,37 @@ public class Indicators : MonoBehaviour {
     void Update () {
         gamepad = GamePad.GetState(0);
 
-        if (gamepad.Buttons.A == ButtonState.Pressed)
-            indicators[0].SetActive(true);
-        else
-            indicators[0].SetActive(false);
+        if (Globals.applicationMode == Globals.ApplicationMode.Playing && !Globals.bot)
+        {
+            if (gamepad.Buttons.A == ButtonState.Pressed)
+                indicators[0].SetActive(true);
+            else
+                indicators[0].SetActive(false);
 
-        if (gamepad.Buttons.B == ButtonState.Pressed)
-            indicators[1].SetActive(true);
-        else
-            indicators[1].SetActive(false);
+            if (gamepad.Buttons.B == ButtonState.Pressed)
+                indicators[1].SetActive(true);
+            else
+                indicators[1].SetActive(false);
 
-        if (gamepad.Buttons.Y == ButtonState.Pressed)
-            indicators[2].SetActive(true);
-        else
-            indicators[2].SetActive(false);
+            if (gamepad.Buttons.Y == ButtonState.Pressed)
+                indicators[2].SetActive(true);
+            else
+                indicators[2].SetActive(false);
 
-        if (gamepad.Buttons.X == ButtonState.Pressed)
-            indicators[3].SetActive(true);
-        else
-            indicators[3].SetActive(false);
+            if (gamepad.Buttons.X == ButtonState.Pressed)
+                indicators[3].SetActive(true);
+            else
+                indicators[3].SetActive(false);
 
-        if (gamepad.Buttons.LeftShoulder == ButtonState.Pressed)
-            indicators[4].SetActive(true);
+            if (gamepad.Buttons.LeftShoulder == ButtonState.Pressed)
+                indicators[4].SetActive(true);
+            else
+                indicators[4].SetActive(false);
+        }
         else
-            indicators[4].SetActive(false);
+        {
+            foreach (GameObject indicator in indicators)
+                indicator.SetActive(false);
+        }
     }
 }
