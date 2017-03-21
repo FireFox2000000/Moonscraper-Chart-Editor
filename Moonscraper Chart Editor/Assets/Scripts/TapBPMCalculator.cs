e6a411b948c7c07b712a10650523a00d51ad6820
Alexander Ong <FireFox2000000@gmail.com>
2017-03-21 12:55:31 +1100

    Added TapBPMCalculator class to calculate BPMs once implemented with proper UI.

---
+++ Moonscraper Chart Editor/Assets/Scripts/TapBPMCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapBPMCalculator {
+    const float RESET_TIME = 2.0f;
+
+    float bpm
+    {
+        get
+        {
+            if (numOfTaps != 0)
+                return (lastTapTime - initTapTime) / numOfTaps * 60.0f;
+            else
+                return 0;
+        }
+    }
+
+    float lastTapTime = 0;
+    float initTapTime = 0;
+    float numOfTaps = 0;
+	
+    public void Tap()
+    {
+        if (Time.realtimeSinceStartup - lastTapTime >= RESET_TIME)
+        {
+            Reset();
+        }
+
+        lastTapTime = Time.realtimeSinceStartup;
+        ++numOfTaps;
+    }
+
+    public void Reset()
+    {
+        initTapTime = Time.realtimeSinceStartup;
+        lastTapTime = Time.realtimeSinceStartup;
+        numOfTaps = 0;
+    }
+}
