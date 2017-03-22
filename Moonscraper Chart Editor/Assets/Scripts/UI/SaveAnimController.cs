0bc108f24ea0b33edccbf52cd3f3666bf1d9dd49
Alexander Ong <FireFox2000000@gmail.com>
2017-03-22 12:19:30 +1100

    Added small saving notification.

---
+++ Moonscraper Chart Editor/Assets/Scripts/UI/SaveAnimController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveAnimController : MonoBehaviour {
+    public Text saveText;
+    ChartEditor editor;
+
+    bool fadein = false;
+    float alpha = 0;
+    public float fadeSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+        editor = ChartEditor.FindCurrentEditor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (editor.currentSong.IsSaving)
+            fadein = true;
+        else if (alpha >= 1)
+            fadein = false;
+
+        if (fadein)
+            alpha += fadeSpeed * Time.deltaTime;
+        else
+            alpha -= fadeSpeed * Time.deltaTime;
+
+        alpha = Mathf.Clamp01(alpha);
+
+        saveText.color = new Color(saveText.color.r, saveText.color.g, saveText.color.b, alpha);
+
+        
+	}
+}
