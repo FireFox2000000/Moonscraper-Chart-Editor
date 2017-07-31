9e98274f996e0a453f0c06311d1b79e990d1066d
Alexander Ong <FireFox2000000@gmail.com>
2017-07-31 17:41:11 +1000

    Added drop shadows to menu dropdowns.

---
+++ Moonscraper Chart Editor/Assets/Scripts/UI/DropshadowSizing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropshadowSizing : MonoBehaviour {
+    [SerializeField]
+    CustomUnityDropdown dropdown;
+    [SerializeField]
+    RectTransform item;
+    [SerializeField]
+    Vector2 offset = new Vector2(5, -7);
+    RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 size = rectTransform.sizeDelta;
+        size.x = item.rect.width;
+        size.y = item.rect.height * dropdown.options.Count;
+        rectTransform.sizeDelta = size;
+
+        Vector2 position = item.position;
+
+        position += offset;
+        rectTransform.position = position;
+
+        
+    }
+}
