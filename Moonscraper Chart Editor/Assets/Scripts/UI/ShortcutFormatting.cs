88a6795cab1ddc6797cb62b2ec3fcc0bf0c499b3
Alexander Ong <FireFox2000000@gmail.com>
2018-03-17 18:15:57 +1100

    Implemented shortcuts menu

---
+++ Moonscraper Chart Editor/Assets/Scripts/UI/ShortcutFormatting.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ShortcutFormatting : MonoBehaviour {
+    [SerializeField]
+    TextAsset shortcutText;
+    [SerializeField]
+    Text headingTextField;
+    [SerializeField]
+    Text keysTextField;
+    [SerializeField]
+    Text valuesTextField;
+
+    const char HEADER_ID = '\t';
+    const char KEYVALUE_SPLIT_ID = '|';
+
+    // Use this for initialization
+    void Start () {
+        SetStrings();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Application.isPlaying)
+            SetStrings();
+    }
+
+    void SetStrings()
+    {
+        StringBuilder headerSb = new StringBuilder();
+        StringBuilder keysSb = new StringBuilder();
+        StringBuilder valuesSb = new StringBuilder();
+
+        string[] lines = shortcutText.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
+        foreach (string line in lines)
+        {
+            if (line.Length > 1)
+            {
+                if (line[0] != HEADER_ID)
+                    headerSb.Append(line.Trim());
+                else
+                {
+                    string[] keyValueSplit = line.Split(KEYVALUE_SPLIT_ID);
+                    if (keyValueSplit.Length != 2)
+                        continue;
+
+                    string key = keyValueSplit[0].Trim();
+                    string value = keyValueSplit[1].Trim();
+
+                    keysSb.AppendFormat("{0} - ", key);
+                    valuesSb.Append(value);
+                }
+            }
+
+            const char LINE_BREAK = '\n';
+            headerSb.Append(LINE_BREAK);
+            keysSb.Append(LINE_BREAK);
+            valuesSb.Append(LINE_BREAK);
+        }
+
+        headingTextField.text = headerSb.ToString();
+        keysTextField.text = keysSb.ToString();
+        valuesTextField.text = valuesSb.ToString();
+    }
+}
