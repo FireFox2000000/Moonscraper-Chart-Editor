b69b536da204d76090dda04140edb16c7a871827
Alexander Ong <FireFox2000000@gmail.com>
2018-06-02 15:46:16 +1000

    Cleanup

--- Moonscraper Chart Editor/Assets/Scripts/Gameplay/Rules/GameplayInputFunctions.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Gameplay/Rules/GameplayInputFunctions.cs
@@ -5,10 +5,7 @@ public static class GameplayInputFunctions  {
     {
         if (inputMask == 0)
         {
-            if (note.fret_type == Note.Fret_Type.OPEN)
-                return true;
-            else
-                return false;
+            return note.fret_type == Note.Fret_Type.OPEN;
         }
         else
         {
@@ -18,10 +15,7 @@ public static class GameplayInputFunctions  {
                 // Regular chords
                 if (noteStreak == 0 || note.type == Note.Note_Type.Strum)
                 {
-                    if (inputMask == note.mask)
-                        return true;
-                    else
-                        return false;
+                    return inputMask == note.mask;
                 }
                 // HOPO or tap chords. Insert Exile chord anchor logic.
                 else
@@ -40,20 +34,14 @@ public static class GameplayInputFunctions  {
 
                     shiftedInputMask >>= shiftCount;
 
-                    if (shiftedInputMask == shiftedNoteMask)
-                        return true;
-                    else
-                        return false;
+                    return shiftedInputMask == shiftedNoteMask;
                 }
             }
             // Single notes
             else
             {
                 int singleNoteInput = inputMask >> (int)note.fret_type;     // Anchor logic
-                if (singleNoteInput == 1)
-                    return true;
-                else
-                    return false;
+                return singleNoteInput == 1;
             }
         }
     }
@@ -73,50 +61,4 @@ public static class GameplayInputFunctions  {
 
         }
     }
-
-    // Todo- Keyboard controls and figure out kick input
-    /*
-    public static int GetDrumsInputMask(GamePadState? gamePad)
-    {
-        int inputMask = 0;
-
-        if (GameplayManager.gamepad != null)
-        {
-            GamePadState gamepad = (GamePadState)GameplayManager.gamepad;
-
-            if (gamepad.Buttons.A == ButtonState.Pressed)
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.GREEN;
-
-            if (gamepad.Buttons.B == ButtonState.Pressed)
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.RED;
-
-            if (gamepad.Buttons.Y == ButtonState.Pressed)
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.YELLOW;
-
-            if (gamepad.Buttons.X == ButtonState.Pressed)
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.BLUE;
-
-            if (gamepad.Buttons.LeftShoulder == ButtonState.Pressed)
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.ORANGE;
-
-            //if (gamepad.Buttons.RightShoulder == ButtonState.Pressed)
-            //    inputMask |= 1 << (int)Note.Drum_Fret_Type.KICK;
-        }
-        else
-        {
-            const int drumsKey = 0;
-
-            // Keyboard controls
-            for (int i = 0; i < 5; ++i)
-            {
-                if (Input.GetKey((i + 1).ToString()))
-                    inputMask |= 1 << i;
-            }
-        
-            if (Input.GetKey((drumsKey).ToString()))
-                inputMask |= 1 << (int)Note.Drum_Fret_Type.KICK;
-        }
-
-        return inputMask;
-    }*/
 }
