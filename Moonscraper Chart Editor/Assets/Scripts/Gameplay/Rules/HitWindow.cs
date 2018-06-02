5abd65cb44e0e0932be007a05091fc27b1e466d6
Alexander Ong <FireFox2000000@gmail.com>
2018-06-02 15:46:45 +1000

    Add debug statement to catch any potential bad note ordering

--- Moonscraper Chart Editor/Assets/Scripts/Gameplay/Rules/HitWindow.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Gameplay/Rules/HitWindow.cs
@@ -52,6 +52,12 @@ public class HitWindow<TNoteHitKnowledge> where TNoteHitKnowledge : NoteHitKnowl
         TNoteHitKnowledge newNoteKnowledge = System.Activator.CreateInstance(typeof(TNoteHitKnowledge), note) as TNoteHitKnowledge;
         m_noteQueue.Add(newNoteKnowledge);
 
+        if (m_noteQueue.Count > 1 && m_noteQueue[m_noteQueue.Count - 1].note.position <= m_noteQueue[m_noteQueue.Count - 2].note.position)
+        {
+            Debug.LogError("Notes inserted into hit window in the wrong order");
+        }
+
+
         return true;
     }
 
