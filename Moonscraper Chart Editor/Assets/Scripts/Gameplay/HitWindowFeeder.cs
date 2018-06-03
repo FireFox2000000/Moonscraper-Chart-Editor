48a2743e7f20a65672d4c11eebcda0e8213ad284
Alexander Ong <FireFox2000000@gmail.com>
2018-06-03 20:20:12 +1000

    Rename hit window manager

---
+++ Moonscraper Chart Editor/Assets/Scripts/Gameplay/HitWindowFeeder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitWindowFeeder : MonoBehaviour {
+
+    [HideInInspector]
+    public HitWindow<GuitarNoteHitKnowledge> hitWindow = new HitWindow<GuitarNoteHitKnowledge>(GuitarGameplayConfig.frontendHitWindowTime, GuitarGameplayConfig.backendHitWindowTime);
+    List<NoteController> physicsWindow = new List<NoteController>();
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        NoteController nCon = col.gameObject.GetComponentInParent<NoteController>();
+        if (nCon && !nCon.hit && !physicsWindow.Contains(nCon))
+        {
+            // We only want 1 note per position so that we can compare using the note mask
+            foreach (NoteController insertedNCon in physicsWindow)
+            {
+                if (nCon.note.position == insertedNCon.note.position)
+                    return;
+            }
+
+            // Insert into sorted position
+            for (int i = 0; i < physicsWindow.Count; ++i)
+            {
+                if (nCon.note < physicsWindow[i].note)
+                {
+                    physicsWindow.Insert(i, nCon);
+                    return;
+                }
+            }
+
+            physicsWindow.Add(nCon);
+        }
+    }
+
+    void Update()
+    {
+        float time = ChartEditor.GetInstance().currentVisibleTime;
+
+        // Enter window
+        foreach (NoteController note in physicsWindow.ToArray())
+        {
+            if (hitWindow.DetectEnter(note.note, time))
+            {
+                physicsWindow.Remove(note);
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        hitWindow.noteKnowledgeQueue.Clear();
+        physicsWindow.Clear();
+    }
+}
