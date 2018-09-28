b46bd72810a7e705c9033f0b384d0b6d623449ca
Alexander Ong <FireFox2000000@gmail.com>
2018-09-28 11:05:57 +1000

    Batch beat lines into a single draw call.

---
+++ Moonscraper Chart Editor/Assets/Scripts/Visuals/SpriteBillboarder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBillboarder : MonoBehaviour {
+    public bool oneShot = true;
+    public Camera billboardCamera;
+
+	// Use this for initialization
+	void Start () {
+        ApplyBillboardingRotation();
+        DisableIfOneShot();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ApplyBillboardingRotation();
+        DisableIfOneShot();
+    }
+
+    void DisableIfOneShot()
+    {
+        if (oneShot)
+            enabled = false;
+    }
+
+    void ApplyBillboardingRotation()
+    {
+        if (!billboardCamera)
+            billboardCamera = Camera.main;
+
+        transform.rotation = billboardCamera.transform.rotation;
+    }
+}
