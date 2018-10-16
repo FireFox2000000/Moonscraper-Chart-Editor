0d3f21e0b9c71c90bc6612df39db869ddd13e16f
Alexander Ong <FireFox2000000@gmail.com>
2018-10-16 19:18:53 +1100

    Fixed export menu scroll view not anchored to aspect ratio properly + debug functions and extra test script.

---
+++ Moonscraper Chart Editor/Assets/Scripts/Debug/DebugStringFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugStringFunctions {
+
+    public static bool ValidateString(string str, out System.Exception exceptionResult)
+    {
+        exceptionResult = null;
+
+        try
+        {
+            System.Text.StringBuilder saveString = new System.Text.StringBuilder();
+            string s_sectionFormat = " = E \"section {0}\"";
+            saveString.AppendFormat(s_sectionFormat, str);
+        }
+        catch (System.Exception e)
+        {
+            exceptionResult = e;
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Moonscraper Chart Editor/Assets/Scripts/Debug/Tests/DisplayMenuCrashTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMenuCrashTest : MonoBehaviour {
+    Export exportMenu;
+    bool running = false;
+
+	// Use this for initialization
+	void Start () {
+        exportMenu = ChartEditor.Instance.uiServices.GetComponentInChildren<Export>(true);
+        Debug.Assert(exportMenu, "Unable to locate Export menu");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        try
+        {
+            if (exportMenu.gameObject.activeSelf)
+                exportMenu.Disable();
+            else
+                ChartEditor.Instance.EnableMenu(exportMenu);
+        }
+        catch (System.Exception e)
+        {
+            Logger.LogException(e, "DisplayMenuCrashTest caught error");
+            enabled = false;
+        }
+	}
+}
