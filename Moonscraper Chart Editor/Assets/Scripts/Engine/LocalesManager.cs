5c64e6c020cbfd9fdaa289c9f8ce1a02e45dc91d
FireFox <FireFox2000000@gmail.com>
2019-02-03 12:46:27 +1100

    Account for .Net 4.0 changing decimal sperator based on OS locale.
    # Conflicts:
    #	Moonscraper Chart Editor/Assets/Scripts/Engine/LocalesManager.cs

--- Moonscraper Chart Editor/Assets/Scripts/Engine/LocalesManager.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/LocalesManager.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public static class LocalesManager
 {
-    static char m_decimalSeperator = '.';
-    public static char decimalSeperator { get { return m_decimalSeperator; } }
+    static string m_decimalSeperatorStr = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+    public static char decimalSeperator { get; } = m_decimalSeperatorStr.Length > 0 ? m_decimalSeperatorStr[0] : '.';
 }
