0b198b743632d04bd1b3b755a87b8e3d30eea67e
FireFox <FireFox2000000@gmail.com>
2019-09-08 09:33:31 +1000

    Automatically populate credits screen from attribution.txt

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/PopulateUITextFromTextAsset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Text))]
+public class PopulateUITextFromTextAsset : MonoBehaviour
+{
+    [SerializeField]
+    TextAsset textAsset;
+
+    Text uiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiText = GetComponent<Text>();
+        PopulateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            PopulateUI();
+        }
+    }
+
+    void PopulateUI()
+    {
+        if (textAsset)
+        {
+            uiText.text = textAsset.text;
+        }
+    }
+}
