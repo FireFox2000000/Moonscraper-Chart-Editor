b89fa625f4b80060f9310eda53393bbc66f78e95
FireFox <FireFox2000000@gmail.com>
2020-05-06 08:56:47 +1000

    Added theoretical linux versions of file explorer, message box and window handle. Incomplete labeled with // #LINUX TODO

---
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/MessageBox/NativeMessageBoxLinux.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_STANDALONE_LINUX
+
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+public class NativeMessageBoxLinux : INativeMessageBox
+{
+    public NativeMessageBox.Result Show(string text, string caption, NativeMessageBox.Type messageBoxType, NativeWindow childWindow)
+    {
+        IntPtr messagePtr = IntPtr.Zero;
+
+        if (childWindow != null)
+        {
+            NativeWindow_Linux winInterface = childWindow.GetInterface() as NativeWindow_Linux;
+
+            UnityEngine.Debug.Assert(winInterface != null);
+
+            messagePtr = winInterface.sdlWindowPtr;
+        }
+
+        return NativeMessageBoxSDL.Show(text, caption, messageBoxType, messagePtr);
+    }
+}
+
+#endif
