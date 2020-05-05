048429f66c5aefe830d4fac7cd93575c41cb7dae
FireFox <FireFox2000000@gmail.com>
2020-05-06 08:56:47 +1000

    Added theoretical linux versions of file explorer, message box and window handle. Incomplete labeled with // #LINUX TODO

---
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_Linux.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_STANDALONE_LINUX
+
+using System.Runtime.InteropServices;
+using System;
+
+public class NativeWindow_Linux : NativeWindow_SDL, INativeWindow
+{
+    public bool SetApplicationWindowPointerByName(string desiredWindowName)
+    {
+        // #LINUX TODO
+        // Need to get a window pointer that is compatible with SDL_CreateWindowFrom
+        // Potential solution- https://stackoverflow.com/questions/42449050/cant-get-a-window-handle
+        throw new NotImplementedException();
+    }
+}
+
+#endif
