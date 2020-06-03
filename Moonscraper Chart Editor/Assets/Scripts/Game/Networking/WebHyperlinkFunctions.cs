591cf592b5ed775e4db0f8cc740615ef30f80135
FireFox <FireFox2000000@gmail.com>
2020-06-04 09:11:00 +1000

    Replaced text in contact menu with direct hyperlinks. Remove Twitter info and replaced it with github link cause I rarely ever check that one.

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/Networking/WebHyperlinkFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebHyperlinkFunctions : MonoBehaviour
+{
+    public void OpenLink(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
