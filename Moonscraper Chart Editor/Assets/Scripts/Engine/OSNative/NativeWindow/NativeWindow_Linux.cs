54f3b6549a527b12f71628ff9edb4bf59f9e2c86
FireFox <FireFox2000000@gmail.com>
2020-06-27 10:38:09 +1000

    Fixed merge.

--- Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_Linux.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_Linux.cs
@@ -3,7 +3,6 @@
 using System.Runtime.InteropServices;
 using System;
 
-<<<<<<< HEAD
 public class NativeWindow_Linux : INativeWindow
 {
     public bool IsConnectedToWindow()
@@ -11,26 +10,18 @@ public class NativeWindow_Linux : INativeWindow
         throw new NotImplementedException();
     }
 
-=======
-public class NativeWindow_Linux : NativeWindow_SDL, INativeWindow
-{
->>>>>>> 1c48aff03f6a4267d0f0235df6040af3a83722cc
     public bool SetApplicationWindowPointerByName(string desiredWindowName)
     {
         // #LINUX TODO
         // Need to get a window pointer that is compatible with SDL_CreateWindowFrom
         // Potential solution- https://stackoverflow.com/questions/42449050/cant-get-a-window-handle
         throw new NotImplementedException();
-<<<<<<< HEAD
     }
 
     public void SetWindowTitle(string title)
     {
         throw new NotImplementedException();
     }
-=======
-    }
->>>>>>> 1c48aff03f6a4267d0f0235df6040af3a83722cc
 }
 
 #endif
