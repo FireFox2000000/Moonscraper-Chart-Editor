d7a076ff95028fe6738772ee576360d383990a21
FireFox <FireFox2000000@gmail.com>
2020-06-27 10:28:22 +1000

    Removed SDL implementation of Message Box and Window Handle.

--- Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_Linux.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_Linux.cs
@@ -3,13 +3,23 @@
 using System.Runtime.InteropServices;
 using System;
 
-public class NativeWindow_Linux : NativeWindow_SDL, INativeWindow
+public class NativeWindow_Linux : INativeWindow
 {
+    public bool IsConnectedToWindow()
+    {
+        throw new NotImplementedException();
+    }
+
     public bool SetApplicationWindowPointerByName(string desiredWindowName)
     {
         // #LINUX TODO
         // Need to get a window pointer that is compatible with SDL_CreateWindowFrom
         // Potential solution- https://stackoverflow.com/questions/42449050/cant-get-a-window-handle
+        throw new NotImplementedException();
+    }
+
+    public void SetWindowTitle(string title)
+    {
         throw new NotImplementedException();
     }
 }
