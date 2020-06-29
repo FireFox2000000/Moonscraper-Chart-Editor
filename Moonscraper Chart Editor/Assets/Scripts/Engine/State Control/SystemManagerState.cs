3c8b302e115d056382ba1e02d5c90cf43b1d47af
FireFox <FireFox2000000@gmail.com>
2020-06-29 22:09:59 +1000

    Clear out registered systems upon exit to prevent leaks when reusing states.

--- Moonscraper Chart Editor/Assets/Scripts/Engine/State Control/SystemManagerState.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/State Control/SystemManagerState.cs
@@ -77,6 +77,7 @@ namespace MoonscraperEngine
                 system.SystemExit();
 
             hasEntered = false;
+            registeredSystems.Clear();
         }
 
         public virtual void Update()
