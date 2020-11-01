48e5a3103d83dce02889131c3c4c1d399f407685
FireFox <FireFox2000000@gmail.com>
2020-11-01 11:53:37 +1100

    Added stats menu as a sub of the display properties menu

--- Moonscraper Chart Editor/Assets/Scripts/Game/UI/Inspectors/DisplayProperties.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/Inspectors/DisplayProperties.cs
@@ -7,10 +7,14 @@ using System.Collections.Generic;
 
 public class DisplayProperties : UpdateableService
 {
+    public Button menuTabButton;
+    public RectTransform settingsMenuOption;
+    public RectTransform statsMenuOption;
+
+    [Header("Settings")]
     public Text songNameText;
     public Slider hyperspeedSlider;
-    public InputField snappingStep;
-    public Text noteCount;
+    public InputField snappingStep;  
     public Text gameSpeed;
     public Slider gameSpeedSlider;
     public Toggle clapToggle; 
@@ -21,9 +25,35 @@ public class DisplayProperties : UpdateableService
     [SerializeField]
     BGFadeHeightController bgFade;
 
+    [Header("Stats")]
+    public Text noteCount;
+    public Text spCount;
+    public Text localEventsCount;
+    public Text bpmCount;
+    public Text tsCount;
+    public Text sectionCount;
+    public Text globalEventsCount;
+
     ChartEditor editor;
-    int prevNoteCount = -1;
+
+    // Cache whether we need to modify strings or not
     int prevSnappingStep = 16;
+    int prevNoteCount = -1;
+    int prevSpCount = -1;
+    int prevLocalEventCount = -1;
+    int prevBpmCount = -1;
+    int prevTsCount = -1;
+    int prevSecCount = -1;
+    int prevGlobalEventCount = -1;
+
+    enum MenuTabOptions
+    {
+        Settings,
+        Stats,
+    }
+
+    MenuTabOptions currentMenuTab = MenuTabOptions.Settings;
+    const string c_menuTabTextFormat = "< {0} >";
 
     protected override void Start()
     {
@@ -50,21 +80,61 @@ public class DisplayProperties : UpdateableService
     {
         clapToggle.isOn = Globals.gameSettings.clapEnabled;
         metronomeToggle.isOn = Globals.gameSettings.metronomeActive;
+
+        UpdateMenuTabDisplay();
     }
 
     public override void OnServiceUpdate()
     {
-        if (editor.currentChart.note_count != prevNoteCount)
-            noteCount.text = "Notes: " + editor.currentChart.note_count.ToString();
+        var currentSong = editor.currentSong;
+        var currentChart = editor.currentChart;
 
-        if (Globals.gameSettings.step != prevSnappingStep)
-            UpdateSnappingStepText();
+        // Update strings
+        {
+            int currentNoteCount = currentChart.note_count;
+            int currentSpCount = currentChart.starPower.Count;
+            int currentLocalEventCount = currentChart.events.Count;
+            int currentBpmCount = currentSong.bpms.Count;
+            int currentTsCount = currentSong.timeSignatures.Count;
+            int currentSecCount = currentSong.sections.Count;
+            int currentGlobalEventCount = currentSong.events.Count;
+
+            if (currentNoteCount != prevNoteCount)
+                noteCount.text = "Notes: " + currentNoteCount.ToString();
+
+            if (currentSpCount != prevSpCount)
+                spCount.text = "Starpower: " + currentSpCount.ToString();
+
+            if (currentLocalEventCount != prevLocalEventCount)
+                localEventsCount.text = "Local Events: " + currentLocalEventCount.ToString();
+
+            if (currentBpmCount != prevBpmCount)
+                bpmCount.text = "BPM: " + currentBpmCount.ToString();
+
+            if (currentTsCount != prevTsCount)
+                tsCount.text = "Timesignatures: " + currentTsCount.ToString();
+
+            if (currentSecCount != prevSecCount)
+                sectionCount.text = "Sections: " + currentSecCount.ToString();
+
+            if (currentGlobalEventCount != prevGlobalEventCount)
+                globalEventsCount.text = "Global Events: " + currentGlobalEventCount.ToString();
+
+            if (Globals.gameSettings.step != prevSnappingStep)
+                UpdateSnappingStepText();
+
+            prevNoteCount = currentNoteCount;
+            prevSpCount = currentSpCount;
+            prevLocalEventCount = currentLocalEventCount;
+            prevBpmCount = currentBpmCount;
+            prevTsCount = currentTsCount;
+            prevSecCount = currentSecCount;
+            prevGlobalEventCount = currentGlobalEventCount;
+        }
 
         // Shortcuts
         if (MSChartEditorInput.GetInputDown(MSChartEditorInputActions.ToggleClap))
             clapToggle.isOn = !clapToggle.isOn;
-
-        prevNoteCount = editor.currentChart.note_count;
     }
 
     void OnChartReload()
@@ -184,5 +254,39 @@ public class DisplayProperties : UpdateableService
 
         Globals.gameSettings.step = stepVal;
         UpdateSnappingStepText();
+    }
+
+    public void NextMenuTab()
+    {
+        currentMenuTab += 1;
+        if ((int)currentMenuTab >= MoonscraperEngine.EnumX<MenuTabOptions>.Count)
+        {
+            currentMenuTab = 0;
+        }
+
+        UpdateMenuTabDisplay();
+    }
+
+    void UpdateMenuTabDisplay()
+    {
+        menuTabButton.GetComponentInChildren<Text>().text = string.Format(c_menuTabTextFormat, currentMenuTab.ToString());
+
+        statsMenuOption.gameObject.SetActive(false);
+        settingsMenuOption.gameObject.SetActive(false);
+
+        switch (currentMenuTab)
+        {
+            case MenuTabOptions.Stats:
+                {
+                    statsMenuOption.gameObject.SetActive(true);
+                    break;
+                }
+            case MenuTabOptions.Settings:
+            default:
+                {
+                    settingsMenuOption.gameObject.SetActive(true);
+                    break;
+                }
+        }
     }
 }
