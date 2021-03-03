1fd2e7ed8a8728dbd8d8c1aa0aeb8898f0612c0c
SpookySquidward <spookysquidward47@gmail.com>
2021-03-02 20:41:54 -0600

    Add LyricEditor2Event Class
    Acts as an interface for lyric phrases; handles all interactions with chart editor events

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Event.cs
@@ -0,0 +1,51 @@
+﻿using MoonscraperChartEditor.Song;
+using System.Collections.Generic;
+
+public class LyricEditor2Event : UnityEngine.MonoBehaviour
+{
+    static string c_phraseStartKeyword = "phrase_start";
+    static string c_phraseEndKeyword = "phrase_end";
+    static string c_lyricPrefix = "lyric ";
+
+    private Event referencedEvent;
+    public bool hasBeenPlaced {get; private set;}
+    public string text {get; private set;}
+
+
+    public LyricEditor2Event (string text) {
+        this.text = text;
+        this.hasBeenPlaced = false;
+    }
+
+    public LyricEditor2Event(Event existingEvent) {
+        referencedEvent = existingEvent;
+        this.text = existingEvent.title;
+        this.hasBeenPlaced = true;
+    }
+
+
+    public void SetText (string newText) {
+        this.text = newText;
+        if (referencedEvent != null) {
+            this.SetTime(referencedEvent.tick);
+        }
+    }
+
+    public void SetTime (uint tick) {
+        List<SongEditCommand> commands = new List<SongEditCommand>();
+
+        if (this.referencedEvent != null)
+        {
+            commands.Add(new SongEditDelete(this.referencedEvent));
+        }
+
+        Event newLyric = new Event(this.text, tick);
+        commands.Add(new SongEditAdd(newLyric));
+
+        BatchedSongEditCommand batchedCommands = new BatchedSongEditCommand(commands);
+        ChartEditor.Instance.commandStack.Push(batchedCommands);
+
+        this.referencedEvent = newLyric;
+        this.hasBeenPlaced = true;
+    }
+}
