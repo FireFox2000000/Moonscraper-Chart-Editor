cea3c784384ed302bd6c1a0ef79216dce7cb79a7
SpookySquidward <spookysquidward47@gmail.com>
2021-03-09 17:15:38 -0600

    Add Lyric Input Menu
    Menu pops up when the user clicks "edit lyrics" and lets them input dash-newline lyrics

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2InputMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LyricEditor2InputMenu : MonoBehaviour
+{
+    [SerializeField]
+    InputField inputField;
+
+    public string text {get {return inputField.text;}}
+
+    public void Display () {
+        gameObject.SetActive(true);
+        inputField.text = null;
+    }
+
+    public void Display (string prefillText) {
+        gameObject.SetActive(true);
+        inputField.text = prefillText;
+    }
+}
