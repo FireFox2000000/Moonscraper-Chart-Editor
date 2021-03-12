f62a76f8b7706f87e0ea94afe86041a357439982
SpookySquidward <spookysquidward47@gmail.com>
2021-03-12 14:11:53 -0600

    Add LyricEditor2AutoScroller class
     Control the scrollbar automatically while the song is playing to scroll along with lyrics

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2AutoScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LyricEditor2AutoScroller : MonoBehaviour
+{
+    [SerializeField]
+    ScrollRect scrollRect;
+    [SerializeField]
+    RectTransform endSpacer;
+
+
+    void Start () {
+
+    }
+
+    void Update () {
+        // Move end spacer to bottom of scroll view
+        endSpacer.SetAsLastSibling();
+    }
+
+    // Smoothly interpolate between two values following the trajectory y=2x-x^2
+    // for 0<x<1
+    static float smoothInterp (float min, float max, float factor) {
+        if (factor < 0) {
+            return min;
+        } else if (factor > 1) {
+            return max;
+        } else {
+            return min + (2 - factor) * factor * (max - min);
+        }
+    }
+}
