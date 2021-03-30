592fcd313dc825340de56c04c08379f8cbdff6d0
SpookySquidward <spookysquidward47@gmail.com>
2021-03-30 17:41:19 -0500

    Disable playback scrolling on lyric input
    For some reason editorStateChangedEvents aren't firing as expected when the user inputs lyrics, so turn off playback scrolling manually on input

--- Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Controller.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/LyricEditor2/LyricEditor2Controller.cs
@@ -212,6 +212,7 @@ public class LyricEditor2Controller : UnityEngine.MonoBehaviour
     public void InputLyrics() {
         PickupAllPhrases();
         ClearPhraseObjects();
+        playbackScrolling = false;
         string inputLyrics = lyricInputMenu.text ?? "";
 
         List<List<string>> parsedLyrics = ParseLyrics(inputLyrics);
