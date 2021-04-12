dfcec9cd6390c2631faa080cdcbd9ffe447d2b19
SpookySquidward <spookysquidward47@gmail.com>
2021-04-11 20:37:25 -0500

    Add BatchedICommand class
    Generic version of BatchedSongEdit command with no logic for forced-note preservation; only for use with self-contained ICommands (e.g. lyric edit commands)

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/Commands/BatchedICommand.cs
@@ -0,0 +1,33 @@
+// Copyright (c) 2016-2020 Alexander Ong
+// See LICENSE in project root for license information.
+
+// Use BatchedSongEditCommand where possible!
+
+using System.Collections.Generic;
+using UnityEngine;
+using MoonscraperEngine;
+
+public class BatchedICommand : ICommand
+{
+    List<ICommand> commands = new List<ICommand>();
+
+    public BatchedICommand(IList<ICommand> newCommands) {
+        foreach (ICommand c in newCommands) {
+            if (c != null) {
+                commands.Add(c);
+            }
+        }
+    }
+
+    public void Invoke() {
+        for (int i = 0; i < commands.Count; i++) {
+            commands[i].Invoke();
+        }
+    }
+
+    public void Revoke() {
+        for (int i = 0; i < commands.Count; i++) {
+            commands[i].Revoke();
+        }
+    }
+}
