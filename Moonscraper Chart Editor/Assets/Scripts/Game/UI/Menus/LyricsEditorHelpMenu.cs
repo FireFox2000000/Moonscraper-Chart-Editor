395427441b1bb7660ba4317056f143d603c363d8
FireFox <FireFox2000000@gmail.com>
2021-05-15 16:13:43 +1000

    Added proper lyric editor help menu rather than a direct video link. for accessibility and contingency in case the video nukes itself since yt is an unpredictable beast.

---
+++ Moonscraper Chart Editor/Assets/Scripts/Game/UI/Menus/LyricsEditorHelpMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LyricsEditorHelpMenu : MonoBehaviour
+{
+    [SerializeField]
+    TextAsset tutorialText;
+    Text textField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<Text>();
+
+        // Populate text field
+        textField.text = tutorialText.text;
+    }
+}
