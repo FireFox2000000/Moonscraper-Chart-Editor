fcadad1df52164ab1b55469326f7ea032f32606b
G_hi3 <marc.forrer@bluewin.ch>
2022-06-04 17:34:48 +0200

    - removed Editor.meta (not necessary) - PhraseValidator: use LyricHelper constants and continue statement on new line - SongValidate: fixed line endings

--- Moonscraper Chart Editor/Assets/Scripts/Game/Misc/PhraseValidator.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/Misc/PhraseValidator.cs
@@ -6,26 +6,24 @@ namespace Game.Misc
 {
     public static class PhraseValidator
     {
-        private const string PhraseStart = "phrase_start";
-        private const string PhraseEnd = "phrase_end";
-        
         public static IEnumerable<IValidationError> GetValidationErrors(Song song)
         {
             Event previousEvent = null;
             foreach (var songEvent in song.events)
             {
                 // Ignores all non-phrase events.
-                if (!(songEvent.title is PhraseStart || songEvent.title is PhraseEnd)) continue;
+                if (!(songEvent.title is LyricHelper.PhraseStartText || songEvent.title is LyricHelper.PhraseEndText))
+                    continue;
 
-                if (songEvent.title is PhraseStart
-                    && previousEvent?.title is PhraseStart)
+                if (songEvent.title is LyricHelper.PhraseStartText
+                    && previousEvent?.title is LyricHelper.PhraseStartText)
                 {
                     yield return new MismatchedPhraseEventError(previousEvent);
                 }
 
                 // previousEvent is only ever null during the first iteration.
-                if (songEvent.title is PhraseEnd
-                    && (previousEvent is null || previousEvent.title is PhraseEnd))
+                if (songEvent.title is LyricHelper.PhraseEndText
+                    && (previousEvent is null || previousEvent.title is LyricHelper.PhraseEndText))
                 {
                     yield return new MismatchedPhraseEventError(songEvent);
                 }
@@ -38,7 +36,7 @@ namespace Game.Misc
                 previousEvent = songEvent;
             }
 
-            if (previousEvent?.title is PhraseStart)
+            if (previousEvent?.title is LyricHelper.PhraseStartText)
             {
                 yield return new MismatchedPhraseEventError(previousEvent);
             }
