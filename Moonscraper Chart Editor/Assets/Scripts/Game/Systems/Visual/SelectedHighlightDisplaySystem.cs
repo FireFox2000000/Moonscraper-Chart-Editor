06ee8b1f4cb08cfc8115c999a48a0b63cbe134d6
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2022-11-29 21:16:00 +1100

    Fixed highlights not rendering over drum rolls properly.

--- Moonscraper Chart Editor/Assets/Scripts/Game/Systems/Visual/SelectedHighlightDisplaySystem.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/Systems/Visual/SelectedHighlightDisplaySystem.cs
@@ -30,8 +30,9 @@ public class SelectedHighlightDisplaySystem : SystemManagerState.System
         }
     }
 	
-	// Update is called once per frame
-	public override void SystemUpdate () {
+    // Update is called once per frame
+    public override void SystemUpdate() 
+    {
         int index, length;
         IList<SongObject> viewRange = editor.selectedObjectsManager.currentSelectedObjects;
         SongObjectHelper.GetRange(viewRange, editor.minPos, editor.maxPos, out index, out length);
@@ -41,36 +42,37 @@ public class SelectedHighlightDisplaySystem : SystemManagerState.System
         bool showHighlight = editor.currentState != ChartEditor.State.Playing && validTool;
 
         int pos = index;
+        int selectedHighlightPoolIndex = 0;
 
-        foreach (GameObject selectedHighlight in selectedHighlightPool)
+        if (showHighlight)
         {
-            if (showHighlight && pos < index + length && viewRange[pos].controller != null && viewRange[pos].controller.gameObject.activeSelf)
+            bool empty = index + length <= 0;
+            int searchEndIndex = empty ? viewRange.Count : pos; // just because our quick search is empty doesn't mean we haven't missed some drum rolls
+            for (int i = 0; i < searchEndIndex && selectedHighlightPoolIndex < selectedHighlightPool.Length; ++i)
             {
-                Collider col3d = viewRange[pos].controller.GetComponent<Collider>();
-                Collider2D col = viewRange[pos].controller.GetComponent<Collider2D>();
-
-                Bounds bounds;
-
-                if (col3d)
+                // Should do this for anything we want to render the whole length for
+                if (viewRange[i].classID == (int)SongObject.ID.DrumRoll)
                 {
-                    bounds = col3d.bounds;
+                    var so = viewRange[i] as DrumRoll;
+                    if ((so.tick + so.length) >= editor.minPos)
+                    {
+                        if (so.controller != null && so.controller.gameObject.activeSelf)
+                        {
+                            RenderHighlight(selectedHighlightPool[selectedHighlightPoolIndex++], so.controller);
+                        }
+                        break;  // Last possible drum roll to render as they don't overlap
+                    }
                 }
-                else
-                {
-                    bounds = col.bounds;
-                }
+            }
+        }
 
-                if (bounds.size.z == 0)
-                {
-                    var size = bounds.size;
-                    size.z = 0.1f;
-                    bounds.size = size;
-                }
-
-                selectedHighlight.transform.localPosition = bounds.center;
-                selectedHighlight.transform.localScale = bounds.size;
-
-                selectedHighlight.SetActive(true);
+        for (; selectedHighlightPoolIndex < selectedHighlightPool.Length; ++selectedHighlightPoolIndex)
+        {
+            GameObject selectedHighlight = selectedHighlightPool[selectedHighlightPoolIndex];
+            bool objectInRange = pos < (index + length);
+            if (showHighlight && objectInRange && viewRange[pos].controller != null && viewRange[pos].controller.gameObject.activeSelf)
+            {
+                RenderHighlight(selectedHighlight, viewRange[pos].controller);
                 ++pos;
             }
             else
@@ -81,6 +83,35 @@ public class SelectedHighlightDisplaySystem : SystemManagerState.System
                 selectedHighlight.SetActive(false);
             }
         }
+    }
+
+    void RenderHighlight(GameObject selectedHighlight, SongObjectController controller)
+    {
+        Collider col3d = controller.GetComponent<Collider>();
+        Collider2D col = controller.GetComponent<Collider2D>();
+
+        Bounds bounds;
+
+        if (col3d)
+        {
+            bounds = col3d.bounds;
+        }
+        else
+        {
+            bounds = col.bounds;
+        }
+
+        if (bounds.size.z == 0)
+        {
+            var size = bounds.size;
+            size.z = 0.1f;
+            bounds.size = size;
+        }
+
+        selectedHighlight.transform.localPosition = bounds.center;
+        selectedHighlight.transform.localScale = bounds.size;
+
+        selectedHighlight.SetActive(true);
     }
 
     public override void SystemExit()
