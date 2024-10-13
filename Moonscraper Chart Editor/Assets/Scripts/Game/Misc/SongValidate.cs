1685e00b6c1e04ca546008ac31a807cf59d25aad
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2024-10-13 14:19:12 +1100

    Added YARG as a seperate validation option and refactored error processing to not double up on rules checks between games that share the same mechanics.

--- Moonscraper Chart Editor/Assets/Scripts/Game/Misc/SongValidate.cs
+++ Moonscraper Chart Editor/Assets/Scripts/Game/Misc/SongValidate.cs
@@ -10,12 +10,16 @@ using Game.Misc;
 
 public class SongValidate
 {
+    // Will crash GH3 if this section limit is exceeded
+    const int GH3_SECTION_LIMIT = 100;
+
     [System.Flags]
     public enum ValidationOptions
     {
         None            = 0,
         GuitarHero3     = 1 << 0,
         CloneHero       = 1 << 1,
+        Yarg            = 1 << 2,
     }
 
     public struct ValidationParameters
@@ -37,15 +41,61 @@ public class SongValidate
 
         sb.AppendFormat("{0}\n", CheckForErrorsMoonscraper(song, validationParams, ref hasErrors));
 
-        if ((validationOptions & ValidationOptions.GuitarHero3) != 0)
+        bool openChordsAllowed = (validationOptions & ValidationOptions.Yarg) != 0;
+        bool openTapsAllowed = (validationOptions & ValidationOptions.Yarg) != 0;
+        int sectionLimit = (validationOptions & ValidationOptions.GuitarHero3) != 0 ? GH3_SECTION_LIMIT : int.MaxValue;
+        bool checkForTSPlacementErrors = (validationOptions & (ValidationOptions.CloneHero | ValidationOptions.Yarg)) != 0;
+        bool checkForMidiSoloSpMisread = validationParams.checkMidiIssues && (validationOptions & (ValidationOptions.CloneHero | ValidationOptions.Yarg)) != 0;
+
+        if (sectionLimit < int.MaxValue)
         {
-            sb.AppendFormat("{0}\n", CheckForErrorsGuitarHero3(song, validationParams, ref hasErrors));
+            StringBuilder errors = new StringBuilder();
+            CheckForSectionLimitErrors(song, sectionLimit, errors);
+            hasErrors |= errors.Length > 0;
+            sb.Append(errors.ToString());
         }
 
-        if ((validationOptions & ValidationOptions.CloneHero) != 0)
+        if (checkForTSPlacementErrors)
         {
-            sb.AppendFormat("{0}\n", CheckForErrorsCloneHero(song, validationParams, ref hasErrors));
+            StringBuilder errors = new StringBuilder();
+            CheckForTimeSignaturePlacementErrors(song, errors);
+            hasErrors |= errors.Length > 0;
+            sb.Append(errors.ToString());
         }
+
+        if (checkForMidiSoloSpMisread)
+        {
+            StringBuilder errors = new StringBuilder();
+            CheckForRockBandMidiSoloStarpowerMisRead(song, errors);
+            hasErrors |= errors.Length > 0;
+            sb.Append(errors.ToString());
+        }
+
+        foreach (var instrument in EnumX<Song.Instrument>.Values)
+        {
+            if (instrument != Song.Instrument.Drums && instrument != Song.Instrument.Unrecognised)
+            {
+                foreach (var difficulty in EnumX<Song.Difficulty>.Values)
+                {
+                    var chart = song.GetChart(instrument, difficulty);
+
+                    if (!openChordsAllowed)
+                    {
+                        var errors = CheckForOpenChords(chart);
+                        hasErrors |= errors.Length > 0;
+                        sb.Append(errors.ToString());
+                    }
+
+                    if (!openTapsAllowed)
+                    {
+                        var errors = CheckForOpenTaps(chart);
+                        hasErrors |= errors.Length > 0;
+                        sb.Append(errors.ToString());
+                    }
+                }
+            }
+        }
+
 
         return sb.ToString();
     }
@@ -131,156 +181,127 @@ public class SongValidate
         return sb.ToString();
     }
 
-    static string CheckForErrorsGuitarHero3(Song song, ValidationParameters validationParams, ref bool hasErrors)
+    static void CheckForSectionLimitErrors(Song song, int sectionLimit, StringBuilder sb)
     {
-        const int SECTION_LIMIT = 100;
-
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("Guitar Hero 3 validation report: ");
-
-        bool hasErrorsLocal = false;
-
-        // Check that we haven't exceeded the section count in GH3
         if (song.sections.Count > 100)
         {
-            hasErrorsLocal |= true;
-            sb.AppendFormat("\tSection count has exceeded limit of {0} sections\n", SECTION_LIMIT);
-            sb.AppendFormat("\tAffected sections:\n");
+            sb.AppendFormat("Section count has exceeded limit of {0} sections\n", sectionLimit);
+            sb.AppendFormat("Affected sections:\n");
 
-            for (int i = SECTION_LIMIT; i < song.sections.Count; ++i)
+            for (int i = sectionLimit; i < song.sections.Count; ++i)
             {
                 Section section = song.sections[i];
-                sb.AppendFormat("\t\tTime = {2}, Position = {0}, Title = {1}\n", section.tick, section.title, PrintObjectTime(section.time));
+                sb.AppendFormat("\tTime = {2}, Position = {0}, Title = {1}\n", section.tick, section.title, PrintObjectTime(section.time));
             }
         }
-
-        if (!hasErrorsLocal)
+    }
+    static void CheckForTimeSignaturePlacementErrors(Song song, StringBuilder sb)
+    {
+        var timeSignatures = song.timeSignatures;
+        for (int tsIndex = 1; tsIndex < timeSignatures.Count; ++tsIndex)
         {
-            sb.AppendLine("\tNo errors detected");
+            TimeSignature previousTs = timeSignatures[tsIndex - 1];
+            TimeSignature.MeasureInfo measureInfo = previousTs.GetMeasureInfo();
+
+            TimeSignature tsToTest = timeSignatures[tsIndex];
+            uint deltaTick = tsToTest.tick - previousTs.tick;
+
+            var measureLine = measureInfo.measureLine;
+            if (((float)deltaTick % measureLine.tickGap) != 0)      // Doesn't line up on a measure
+            {
+                sb.AppendFormat("Found misaligned Time Signature at time {1}, position {0}. Time signatures must be aligned to the measure set by the previous time signature.\n",
+                    tsToTest.tick
+                    , PrintObjectTime(tsToTest.time)
+                );
+            }
         }
-
-        hasErrors |= hasErrorsLocal;
-
-        return sb.ToString();
     }
 
-    static string CheckForErrorsCloneHero(Song song, ValidationParameters validationParams, ref bool hasErrors)
+    static void CheckForRockBandMidiSoloStarpowerMisRead(Song song, StringBuilder sb)
     {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("Clone Hero validation report: ");
-
-        bool hasErrorsLocal = false;
-
-        // Check that time signature positions line up on the measure from the previous time signature
-        {
-            var timeSignatures = song.timeSignatures;
-            for (int tsIndex = 1; tsIndex < timeSignatures.Count; ++tsIndex)
-            {
-                TimeSignature previousTs = timeSignatures[tsIndex - 1];
-                TimeSignature.MeasureInfo measureInfo = previousTs.GetMeasureInfo();
-
-                TimeSignature tsToTest = timeSignatures[tsIndex];
-                uint deltaTick = tsToTest.tick - previousTs.tick;
-
-                var measureLine = measureInfo.measureLine;
-                if (((float)deltaTick % measureLine.tickGap) != 0)      // Doesn't line up on a measure
-                {
-                    hasErrorsLocal |= true;
-                    sb.AppendFormat("\tFound misaligned Time Signature at time {1}, position {0}. Time signatures must be aligned to the measure set by the previous time signature.\n", 
-                        tsToTest.tick
-                        , PrintObjectTime(tsToTest.time)
-                    );
-                }
-            }
-        }
-
-        foreach (var instrument in EnumX<Song.Instrument>.Values)
-        {
-            if (instrument != Song.Instrument.Drums && instrument != Song.Instrument.Unrecognised)
-            {
-                foreach (var difficulty in EnumX<Song.Difficulty>.Values)
-                {
-                    var chart = song.GetChart(instrument, difficulty);
-
-                    for (var i = 0; i < chart.notes.Count; i++)
-                    {
-                        var note = chart.notes[i];
-
-                        if (!note.IsOpenNote())
-                        {
-                            continue;
-                        }
-
-                        bool previousSameTick = note.previous != null && note.tick == note.previous.tick;
-                        bool nextSameTick = note.next != null && note.tick == note.next.tick;
-
-                        // Open chords are not supported in Clone Hero (yet)
-                        if (previousSameTick || nextSameTick)
-                        {
-                            sb.AppendFormat("\tFound Open chord at time {1}, position {0}.\n",
-                                note.tick, PrintObjectTime(note.time));
-
-                            hasErrorsLocal |= true;
-                        }
-
-                        // Neither are Tap opens
-                        if ((note.flags & Note.Flags.Tap) != 0)
-                        {
-                            sb.AppendFormat("\tFound Tap Open note at time {1}, position {0}.\n",
-                                note.tick, PrintObjectTime(note.time));
-
-                            hasErrorsLocal |= true;
-                        }
-                    }
-                }
-            }
-        }
-
         // If we have no starpower but more than 1 solo section then CH will interpret this as an RB1 style midi, and misinterpret the solo markers as starpower
-        if (validationParams.checkMidiIssues)
+        foreach (Song.Instrument instrument in EnumX<Song.Instrument>.Values)
         {
-            foreach (Song.Instrument instrument in EnumX<Song.Instrument>.Values)
+            if (instrument == Song.Instrument.Unrecognised)
+                continue;
+
+            foreach (Song.Difficulty difficulty in EnumX<Song.Difficulty>.Values)
             {
-                if (instrument == Song.Instrument.Unrecognised)
-                    continue;
+                Chart chart = song.GetChart(instrument, difficulty);
 
-                foreach (Song.Difficulty difficulty in EnumX<Song.Difficulty>.Values)
+                if (chart.starPower.Count <= 0)
                 {
-                    Chart chart = song.GetChart(instrument, difficulty);
-
-                    if (chart.starPower.Count <= 0)
+                    // Check for solo markers
+                    int soloMarkerCount = 0;
+                    foreach (ChartEvent ce in chart.events)
                     {
-                        // Check for solo markers
-                        int soloMarkerCount = 0;
-                        foreach (ChartEvent ce in chart.events)
+                        if (ce.eventName == MoonscraperChartEditor.Song.IO.MidIOHelper.SOLO_EVENT_TEXT)
                         {
-                            if (ce.eventName == MoonscraperChartEditor.Song.IO.MidIOHelper.SOLO_EVENT_TEXT)
+                            ++soloMarkerCount;
+
+                            if (soloMarkerCount > 1)
                             {
-                                ++soloMarkerCount;
+                                sb.AppendFormat("Track {0} has no starpower and more than 1 solo section. If exported to the midi format, Clone Hero will interpret this chart as an older style midi, and will misinterpret solo markers as starpower.\n", instrument);
 
-                                if (soloMarkerCount > 1)
-                                {
-                                    hasErrorsLocal |= true;
-                                    sb.AppendFormat("\tTrack {0} has no starpower and more than 1 solo section. If exported to the midi format, Clone Hero will interpret this chart as an older style midi, and will misinterpret solo markers as starpower.\n", instrument);
-
-                                    goto NewInstrument;
-                                }
+                                goto NewInstrument;
                             }
                         }
                     }
                 }
+            }
 
             NewInstrument:;
+        }
+    }
+
+    static StringBuilder CheckForOpenChords(Chart chart)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (var i = 0; i < chart.notes.Count; i++)
+        {
+            var note = chart.notes[i];
+
+            if (!note.IsOpenNote())
+            {
+                continue;
+            }
+
+            bool previousSameTick = note.previous != null && note.tick == note.previous.tick;
+            bool nextSameTick = note.next != null && note.tick == note.next.tick;
+
+            // Open chords are not supported in Clone Hero (yet)
+            if (previousSameTick || nextSameTick)
+            {
+                sb.AppendFormat("Found Open chord at time {1}, position {0}.\n",
+                    note.tick, PrintObjectTime(note.time));
             }
         }
 
-        if (!hasErrorsLocal)
+        return sb;
+    }
+
+    static StringBuilder CheckForOpenTaps(Chart chart)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (var i = 0; i < chart.notes.Count; i++)
         {
-            sb.AppendLine("\tNo errors detected");
+            var note = chart.notes[i];
+
+            if (!note.IsOpenNote())
+            {
+                continue;
+            }
+
+            // Neither are Tap opens
+            if ((note.flags & Note.Flags.Tap) != 0)
+            {
+                sb.AppendFormat("Found Tap Open note at time {1}, position {0}.\n",
+                    note.tick, PrintObjectTime(note.time));
+            }
         }
 
-        hasErrors |= hasErrorsLocal;
-
-        return sb.ToString();
+        return sb;
     }
 }
